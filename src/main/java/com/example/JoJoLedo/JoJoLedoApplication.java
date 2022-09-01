25b951c8a5c458190dca7b9d9b67d287345b25ea
ANDYS_LAPTOP\andyb <andybryanlam@gmail.com>
2022-09-01 09:06:58 +0200

    first commit

---
+++ src/main/java/com/example/JoJoLedo/JoJoLedoApplication.java
@@ -0,0 +1,13 @@
+package com.example.JoJoLedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoJoLedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoJoLedoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JoJoLedo/JoJoLedoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JoJoLedo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoJoLedoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
