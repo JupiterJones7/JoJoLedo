532297911870c761c375406b665d7ac6548aa18f
ANDYS_LAPTOP\andyb <andybryanlam@gmail.com>
2022-09-08 09:04:02 +0200

    Created Stands and Standlist

---
+++ src/main/java/com/example/JoJoLedo/Stand.java
@@ -0,0 +1,67 @@
+package com.example.JoJoLedo;
+
+public class Stand {
+    String standName;
+    String ability;
+    int strength;
+    int speed;
+    int range;
+    int presicion;
+
+    public Stand(String standName, String ability, int strength, int speed, int range, int presicion) {
+        this.standName = standName;
+        this.ability = ability;
+        this.strength = strength;
+        this.speed = speed;
+        this.range = range;
+        this.presicion = presicion;
+    }
+
+    public String getStandName() {
+        return standName;
+    }
+
+    public void setStandName(String standName) {
+        this.standName = standName;
+    }
+
+    public String getAbility() {
+        return ability;
+    }
+
+    public void setAbility(String ability) {
+        this.ability = ability;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public int getPresicion() {
+        return presicion;
+    }
+
+    public void setPresicion(int presicion) {
+        this.presicion = presicion;
+    }
+}
---
+++ src/main/java/com/example/JoJoLedo/Standlist.java
@@ -0,0 +1,18 @@
+package com.example.JoJoLedo;
+
+import java.util.List;
+
+public class Standlist {
+    private List<Stand> stands = List.of(
+            new Stand("Star Platinum", "Super strength", 5, 5, 3, 5),
+            new Stand("Hierophant Green", "Emerald Splash", 3,4, 5, 3),
+            new Stand("Silver Chariot", "Sword fighting", 3,1, 3, 4),
+            new Stand("Magician's Red", "Fire bending", 4,4, 3, 3),
+            new Stand("Hermit Purple", "Spirit image", 2,3, 2, 2),
+            new Stand("The World", "Time stop", 5,5, 3, 5)
+    );
+
+    public List<Stand> getStands() {
+        return stands;
+    }
+}
